5439ac616d4fa46681decf02b74554a3f392eefe
Rohith Nadimpally <35045363+rohithn1@users.noreply.github.com>
2024-09-25 13:34:04 -0700

    Moe activation checkpointing support (#87)
    * adding support for moe specific activation checkpointing when fp8 and smp enabled
    
    * addressing nit
    
    ---------
    
    Co-authored-by: Rohith Nadimpally <rnadimp@amazon.com>

--- src/sagemaker_nemo_adaptor/utils/train_utils.py
+++ src/sagemaker_nemo_adaptor/utils/train_utils.py
@@ -22,6 +22,12 @@ def apply_activation_checkpoint(
     moe: bool = False,
 ):
     """Apply activation checkpoint."""
+    if fp8 and moe and use_smp:
+        # Checkpoint attention and moe layers separately when using FP8 and MoE.
+        # Currently, checkpointing entire TransformerLayer is not supported.
+        apply_activation_checkpoint_moe(model=model)
+        return
+
     from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
         CheckpointImpl,
         apply_activation_checkpointing,
@@ -53,6 +59,54 @@ def apply_activation_checkpoint(
         checkpoint_wrapper, checkpoint_impl=checkpoint_impl, checkpoint_fn=checkpoint_fn
     )
     apply_activation_checkpointing(model, checkpoint_wrapper_fn=entrant_wrapper, check_fn=check_fn_gpt)
+
+
+def apply_activation_checkpoint_moe(model=None, checkpoint_attn=True, checkpoint_moe=True):
+    """
+    Experimental checkpointing with multiple checkpoint wrappers.
+    Use TE checkpoint for attention, and megatron/native checkpoint for MoE layer.
+    """
+    from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
+        CheckpointImpl,
+        apply_activation_checkpointing,
+        checkpoint_wrapper,
+    )
+
+    checkpoint_impl = CheckpointImpl.NO_REENTRANT
+
+    if checkpoint_attn:
+        import torch.sagemaker as tsm
+        import transformer_engine
+        from transformer_engine.pytorch.attention import MultiheadAttention
+
+        check_fn_attn = lambda submodule: isinstance(  # pylint: disable=unnecessary-lambda-assignment
+            submodule, MultiheadAttention
+        )
+        checkpoint_fn_attn = functools.partial(
+            transformer_engine.pytorch.checkpoint,
+            distribute_saved_activations=False,
+            get_rng_state_tracker=tsm.state.get_rng_state_tracker,
+            tp_group=tsm.state.tp_process_group,
+            use_reentrant=False,
+        )
+        # flash attn v2 does not work with no_reentrant
+        # our activation offloading for 2.0 also does not work with no_reentrant
+        entrant_wrapper_attn = functools.partial(
+            checkpoint_wrapper, checkpoint_impl=checkpoint_impl, checkpoint_fn=checkpoint_fn_attn
+        )
+        apply_activation_checkpointing(model, checkpoint_wrapper_fn=entrant_wrapper_attn, check_fn=check_fn_attn)
+
+    if checkpoint_moe:
+        from torch.sagemaker.moe.moe_layer import MoELayer
+
+        check_fn_moe = lambda submodule: isinstance(  # pylint: disable=unnecessary-lambda-assignment
+            submodule, MoELayer
+        )
+        checkpoint_fn_moe = None
+        entrant_wrapper_moe = functools.partial(
+            checkpoint_wrapper, checkpoint_impl=checkpoint_impl, checkpoint_fn=checkpoint_fn_moe
+        )
+        apply_activation_checkpointing(model, checkpoint_wrapper_fn=entrant_wrapper_moe, check_fn=check_fn_moe)
 
 
 def get_batch_for_cp_rank(batch):
