4c21e818de73b25780fb669fb0412c3396abc938
haohanchen-aws <54413235+haohanchen-aws@users.noreply.github.com>
2024-11-22 09:32:20 -0800

    Add Copyrights (#202)
    * add copyrights
    
    * get cfg back
    
    * fix precommit
    
    * a
    
    * add
    
    * remove bash
    
    * remove bash
    
    * check

--- src/hyperpod_nemo_adapter/collections/data/dummy_data_module.py
+++ src/hyperpod_nemo_adapter/collections/data/dummy_data_module.py
@@ -1,3 +1,16 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
 from hyperpod_nemo_adapter.collections.data.base import BaseDataModule
 from hyperpod_nemo_adapter.collections.data.datasets import DummyDataset
 from hyperpod_nemo_adapter.utils.config_utils import get_hf_config_from_name_or_path
